d171ad93436ab74a7079503e282844c6c427abb4
Ceasar Bautista <ceasar.bautista@adroll.com>
2015-01-22 17:47:55 -0800

    add docs

--- elevators.js
+++ elevators.js
@@ -1,44 +1,57 @@
-{
+({
     init: function(elevators, floors) {
         $.each(elevators, function(i, elevator) {
+            // Floors that passengers wish to exit at
             var destinations = {};
+            // Floors that passengers have hit the "Up" button at
             var upPresses = {};
+            // Floors that passengers have hit the "Down" button at
             var downPresses = {};
             $.each(floors, function(i, floor) {
                 destinations[i] = false;
                 upPresses[i] = 0;
                 downPresses[i] = 0;
             });
+
+            // If there are passengers, go to their floors, otherwise, go
+            // to floors where passengers are waiting
             elevator.on("idle", function() {
                 elevator.stop();
                 $.each(floors, function(i, floor) {
-                    if (destinations[i]) {
-                        elevator.goToFloor(i, true);
-                    }
-                    if (upPresses[i] || downPresses[i]) {
+                    if (elevator.loadFactor() > 0) {
+                        if (destinations[i]) {
+                            elevator.goToFloor(i);
+                        }
+                    } else if (upPresses[i] || downPresses[i]) {
                         elevator.goToFloor(i);
                     }
                 });
                 
             });
+
+            // Mark a floor as one to stop at
             elevator.on("floor_button_pressed", function(floorNum) {
                 destinations[floorNum] = true;
             });
+
             elevator.on("passing_floor", function(floorNum, direction) {
                 if (elevator.loadFactor() > 0) {
                     if (destinations[floorNum]) {
                         elevator.goToFloor(floorNum, true);
                     }
                 } else if (upPresses[floorNum] || downPresses[floorNum]) {
-                    elevator.goToFloor(floorNum);
+                    elevator.goToFloor(floorNum, true);
                 }
                 
             });
+
             elevator.on("stopped_at_floor", function(floorNum) {
                 destinations[floorNum] = false;
                 upPresses[floorNum] = 0;
                 downPresses[floorNum] = 0;
             });
+
+            // Record the number of presses
             $.each(floors, function(i, floor) {
                 floor.on("up_button_pressed", function(event) {
                     upPresses[floor.floorNum()] += 1;
@@ -51,4 +64,4 @@
     },
     update: function(dt, elevators, floors) {
     }
-}
+})
