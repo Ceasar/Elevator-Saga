d00b3f062a7d2171fd5972593f1a24f9f51305e7
Ceasar <cbautista2010@gmail.com>
2019-03-03 11:50:50 -0800

    Try to minimize wait time

--- elevators_2.js
+++ elevators_2.js
@@ -1,7 +1,15 @@
 {
   init: function(elevators, floors) {
+    var presses = [];
     var upButtonPressed = {};
     var dnButtonPressed = {};
+
+    function arrRemove(arr, value) {
+      var index = arr.indexOf(value);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+    }
 
     floors.forEach(function(floor) {
       var floorNum = floor.floorNum();
@@ -10,24 +18,64 @@
 
       floor.on("up_button_pressed", function() {
         upButtonPressed[floorNum] = true;
+        presses.push({
+          floorNum: floorNum,
+          direction: "up",
+        });
       });
       floor.on("down_button_pressed", function() {
         dnButtonPressed[floorNum] = true;
+        presses.push({
+          floorNum: floorNum,
+          direction: "down"
+        });
       });
     });
 
     elevators.forEach(function(elevator, elevatorNumber) {
-      elevator.on("idle", function() {
+      function findPassenger() {
+        console.log(elevatorNumber, 'findPassenger', presses);
+        var press = presses.shift();
+        if (press !== undefined) {
+          if (press.direction === "up") {
+            elevator.goingDownIndicator(false);
+          } else {
+            elevator.goingUpIndicator(false);
+          }
+          elevator.goToFloor(press.floorNum);
+          return;
+        }
+      }
+
+      function idle() {
         elevator.goingUpIndicator(true);
         elevator.goingDownIndicator(true);
+
+        findPassenger();
+      }
+
+      floors.forEach(function(floor) {
+        var isIdle = elevator.destinationQueue.length === 0;
+        floor.on("up_button_pressed", function() {
+          if (isIdle) {
+            idle();
+          }
+        });
+        floor.on("down_button_pressed", function() {
+          if (isIdle) {
+            idle();
+          }
+        });
       });
+
+      elevator.on("idle", idle);
 
       elevator.on("floor_button_pressed", function(floorNum) {
         if (elevator.destinationQueue.length === 0) {
           if (floorNum > elevator.currentFloor()) {
-            elevator.goingUpIndicator(true);
+            elevator.goingDownIndicator(false);
           } else {
-            elevator.goingDownIndicator(true);
+            elevator.goingUpIndicator(false);
           }
         }
         elevator.goToFloor(floorNum);
@@ -35,25 +83,24 @@
         if (elevator.goingDownIndicator()) {
           elevator.destinationQueue.reverse();
         }
+        console.log("floor_button_pressed", elevator.destinationQueue);
         elevator.checkDestinationQueue();
       });
 
-      elevator.on("passing_floor", function(floorNum) {
-        var direction = elevator.destinationDirection();
-        if (elevator.loadFactor() < 0.5) {
-          if (direction === "up" && upButtonPressed[floorNum]) {
-            upButtonPressed[floorNum] = false;
-            elevator.goToFloor(floorNum, true);
-          }
-          if (direction === "down" && dnButtonPressed[floorNum]) {
-            dnButtonPressed[floorNum] = false;
-            elevator.goToFloor(floorNum, true);
-          }
-        }
-      });
-
       elevator.on("stopped_at_floor", function(floorNum) {
-        console.log(elevatorNumber, "stopped_at_floor", floorNum);
+        console.log(elevatorNumber, "stopped_at_floor", floorNum, presses);
+        if (elevator.goingUpIndicator()) {
+          arrRemove(presses, {
+            floorNum: floorNum,
+            direction: "up",
+          })
+        }
+        if (elevator.goingDownIndicator()) {
+          arrRemove(presses, {
+            floorNum: floorNum,
+            direction: "down",
+          })
+        }
       });
     });
   },
